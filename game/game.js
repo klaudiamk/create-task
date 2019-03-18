76463150f87269f72de413bd0b206beb9da5e5a5
Klaudia Kedra <kkedra@ucvts.tec.nj.us>
2019-03-18 10:48:14 -0400

    okay

--- game/game.js
+++ game/game.js
@@ -1,22 +1,3 @@
-let fruit = document.getElementById("fruit");
-let container = document.getElementById("container");
-
-let fruitLeft = 0;
-
-function animate(e) {
-  if(e.keyCode == 39) {
-    fruitLeft += 2;
-    fruit.style.left = fruitLeft + 'px'
-  }
-  if(e.keyCode == 37) {
-    fruitLeft -= 2;
-    fruit.style.left = fruitLeft + 'px'
-  if (fruitLeft <= 0) {
-    fruitLeft += 2;
-
-  }
-  }
-//  console.log(e.keyCode);
+function check() {
+  
 }
-
-//document.onkeydown = animate;
